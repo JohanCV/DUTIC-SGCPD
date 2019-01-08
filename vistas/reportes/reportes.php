ad53c2602a1d59563c1121161796c9c4479050f3
Gohan-PC <nahoj1992@gmail.com>
2019-01-08 01:22:41 -0500

    1.6.6(reportes cambios)

--- vistas/reportes/reportes.php
+++ vistas/reportes/reportes.php
@@ -1,7 +1,8 @@
 <h3>REPORTES DE USO DEL AULA VIRTUAL</h3>
 
 <div class="container">
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark" style="
+    width: 980px;">
       <a class="navbar-brand" href="http://dutic.unsa.edu.pe/Reportes/Reporte1.php"
             target="_blank">
           <i class="fas fa-chart-line"></i> Reporte 1
@@ -10,7 +11,8 @@
 </div>
 <br>
 <div class="container">
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark" style="
+    width: 980px;">
         <a class="navbar-brand" href="http://dutic.unsa.edu.pe/Reportes/Reporte2.php"
            target="_blank">
             <i class="fas fa-chart-line"></i> Reporte 2
@@ -19,7 +21,8 @@
 </div>
 <br>
 <div class="container">
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark" style="
+    width: 980px;">
         <a class="navbar-brand" href="http://dutic.unsa.edu.pe/Reportes/Reporte3.php"
            target="_blank">
             <i class="fas fa-chart-line"></i> Reporte 3
@@ -28,7 +31,8 @@
 </div>
 <br>
 <div class="container">
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark" style="
+    width: 980px;">
         <a class="navbar-brand" href="http://dutic.unsa.edu.pe/Reportes/enlace1.php"
            target="_blank">
             <i class="fas fa-chart-line"></i> Enlace 1
